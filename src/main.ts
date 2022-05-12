48b4c87593f0099be8c3b9bc6b2777432dd56f72
Manuel Da Pena <65864237+NachoDPP@users.noreply.github.com>
2022-05-12 10:41:27 -0400

    Observables & Typescript Dependency (#4)
    Se agregó el patrón observador al  dominio donde:
    - DomainEvent (Eventos del dominio)
    - Observable (Clases observables)
    - Observer (Interfaz tipo observador)
    
    Se agregó la dependencia del lenguaje typescript al proyecto a través del Package Manager npm.
    
    Se agregaron los comandos tanto de instalación como compilación del proyecto en el README.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,34 @@
+import { DomainEvent } from "./domain/observables/domain-event";
+import { Observable } from "./domain/observables/observable";
+import { Observer } from "./domain/observables/observer.interface";
+
+export class NuevoObservable extends Observable {
+    run() {
+        for (let i = 0; i < 3; i++) {
+            setTimeout(() => {
+                const event = DomainEvent.create("CambioNombre", { iteración: i });
+                this.notifyAll([event]);
+            }, 1000 + (i * 1000));
+        }
+    }
+}
+
+export class NuevoObservador implements Observer {
+    raise(events: DomainEvent[]) {
+        events.forEach(event => {
+            console.log(event);
+        });
+    }
+}
+
 function main() {
-    console.log("Hello world!");
+    const observable = new NuevoObservable();
+
+    const observador = new NuevoObservador();
+
+    observable.add(observador);
+
+    observable.run();
 }
 
 main();
