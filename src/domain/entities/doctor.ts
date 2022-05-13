ae9882bb8230b5d0d56a8672295977eb248fb04f
Froilan Alejandro Roa Corredor <froilanroac@MacBook-Air-de-Froilan.local>
2022-05-13 19:38:01 -0400

    Correcion de errores en los nombres del doctor y la implementacion del patron factory

--- src/domain/entities/doctor.ts
+++ src/domain/entities/doctor.ts
@@ -7,29 +7,65 @@ import { HoldType } from "../enumerations/hold_type";
 
 
 export class Doctor {
-
-    _id: DoctorId;
-    _firstName: DoctorFirstName;
-    _lastName: DoctorLastName;
-    _specialty: Array<SpecialtyType>;
-    _location: DoctorLocation;
-    _holdType: HoldType;
-
+    
+    
     constructor(
-        id: DoctorId,
-        firstName: DoctorFirstName,
-        lastName: DoctorLastName,
-        specialty: Array<SpecialtyType>,
-        location: DoctorLocation,
-        holdType: HoldType
+        private id: DoctorId,
+        private firstName: DoctorFirstName,
+        private lastName: DoctorLastName,
+        private specialty: Array<SpecialtyType>,
+        private location: DoctorLocation,
+        private holdType: HoldType
     ){
-        this._id = id;
-        this._firstName = firstName;
-        this._lastName = lastName;
-        this._specialty = specialty;
-        this._location = location;
-        this._holdType = holdType;
+        this.validate();
     }
 
+        get Id(){ return this.Id; }
+        get FirstName(){ return this.FirstName; }
+        get LastName(){ return this.LastName; }
+        get Specialty(){ return this.Specialty; }    
+        get Location(){ return this.Location; }
+        get HoldType(){ return this.HoldType; }
+
+        /**
+         * Patron Factory
+         * @param id identificador del doctor
+         * @param firstName primer nombre del doctor
+         * @param lastName apellido del doctor
+         * @param specialty especialidad del doctor
+         * @param location ubicacion del doctor
+         * @param holdType retencion que el doctor pueda tener en el sistema
+         * @returns `Doctor`
+         */
+        public static create(id: DoctorId,
+             firstName: DoctorFirstName, 
+             lastName: DoctorLastName, 
+             specialty: Array<SpecialtyType>, 
+             location: DoctorLocation, 
+             holdType: HoldType): Doctor {
+                 return new Doctor(id, firstName, lastName,specialty,location,holdType);
+             }
+
+
+             private validate():void {
+                if (this.id == null || this.id == undefined){
+                    throw new Error("El Id del doctor no puede ser null/undefined");
+                }
+                if (this.firstName == null || this.firstName == undefined){
+                    throw new Error("El primer nombre del doctor no puede ser null/undefined");
+                }
+                if (this.lastName == null || this.lastName == undefined){
+                    throw new Error("El apellido del doctor no puede ser null/undefined");
+                }
+                if (this.specialty == null || this.specialty == undefined){
+                    throw new Error("la especialidad del doctor no puede ser null/undefined");
+                }
+                if (this.location == null || this.location == undefined){
+                    throw new Error("la ubicacion del doctor no puede ser null/undefined");
+                }
+                if (this.holdType == null || this.holdType == undefined){
+                    throw new Error("la retencion del doctor no puede ser null/undefined");
+                }
+             }
 
 }
