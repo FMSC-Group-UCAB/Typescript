96e7fb032582230e111e9c2d8855fdde0ebd5c20
Carlos Doffiny S-V <cdoffiny@gcf-corp.com>
2022-05-14 08:16:33 -0400

    Creados los value objects que serán usados en la entidad Patient, junto con el tipo enumerado HoldType. Todos los value objects tienen sus getters, equals, patrón factory y code docs

---
+++ src/domain/valueobjects/patient/patient-last-name.ts
@@ -0,0 +1,27 @@
+import { IValueObject } from "src/domain/interfaces/value-object.interface";
+
+/** PatientLastName: Es el Value Object del apellido del paciente.*/
+export class PatientLastName implements IValueObject<PatientLastName> {
+    
+    private constructor(private readonly lastName: string) { }
+
+    //Getter
+    get Value() { return this.lastName; }
+
+
+    equals(other: PatientLastName): boolean {
+        return this.lastName == other.lastName;
+    }
+
+    
+    /**Patron Factory.
+     * @param lastName Apellido del paciente.
+     * @returns `PatientLastName`.*/
+    public static create(lastName: string): PatientLastName {
+        if (lastName == null || lastName == undefined || lastName == "") {
+            throw new Error("El apellido del paciente no puede ser nulo.");
+        }
+
+        return new PatientLastName(lastName);
+    }
+}
---
+++ src/domain/valueobjects/patient/patient-phone-number.ts
@@ -0,0 +1,27 @@
+import { IValueObject } from "src/domain/interfaces/value-object.interface";
+
+/** PatientPhoneNumber: Es el Value Object del numero de telefono del paciente.*/
+export class PatientPhoneNumber implements IValueObject<PatientPhoneNumber> {
+
+    private constructor(private readonly phoneNumber: string) { }
+
+    //Getter
+    get Value() { return this.phoneNumber; }
+
+
+    equals = (other: PatientPhoneNumber): boolean => {
+        return this.phoneNumber == other.phoneNumber;
+    }
+
+
+    /**Patron Factory.
+     * @param phoneNumber Numero de telefono del paciente.
+     * @returns `PatientPhoneNumber`.*/
+    public static create = (phoneNumber: string): PatientPhoneNumber => {
+        if (phoneNumber == null || phoneNumber == undefined || phoneNumber == "") {
+            throw new Error("El numero de telefono del paciente no puede ser nulo.");
+        }
+
+        return new PatientPhoneNumber(phoneNumber);
+    }
+}
